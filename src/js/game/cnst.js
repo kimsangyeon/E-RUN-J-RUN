62e0a1a1cd9f19434be12ca7663acf3cc634b06e
yeon <dho113@naver.com>
2017-12-30 22:53:16 +0900

    e-run-j-run_task/ - Game cast.js 생성

---
+++ src/js/game/cnst.js
@@ -0,0 +1,17 @@
+const cnst = {
+    //Canvas
+    canvasWidth: 480,
+    canvasHeight: 270,
+
+    //Charecter
+    charWidht: 44,
+    charHeight: 44,
+    charX: 0,
+    charY: 226,
+    ticksperFrame: 10,
+    numberOfFrames: 10,
+    jumpingY: 45
+}
+
+export default cnst;
+
