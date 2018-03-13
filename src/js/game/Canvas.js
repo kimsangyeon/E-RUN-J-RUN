d29321c5e0b26fd2b8cc86a3c926f408e1eb03f0
yeon <dho113@naver.com>
2018-03-13 21:57:11 +0900

    e-run-j-run_task/ - 블럭 충돌처리 조건문 수정

--- src/js/game/Canvas.js
+++ src/js/game/Canvas.js
@@ -131,16 +131,16 @@ class Canvas {
         this.block.render();
 
         if (this.block.image.src.indexOf('ice') === -1
-        && (this.charecter.x + this.charecter.width > this.block.x
-        && this.charecter.y + this.charecter.height - this.charecter.gravity > this.block.y
-        && this.charecter.x < this.block.x + this.block.width)) {
+        && (this.charecter.x + this.charecter.width > this.block.x + this.block.width / 2
+        && this.charecter.y + this.charecter.height - this.charecter.gravity > this.block.y + this.block.height / 2
+        && this.charecter.x < this.block.x + this.block.width - this.block.width / 2)) {
             window.cancelAnimationFrame(this.charFrameId);
             window.cancelAnimationFrame(this.blockFrameId);
             window.cancelAnimationFrame(this.coinFrameId);
         } else if (this.block.image.src.indexOf('ice') !== -1
-        && (this.charecter.x + this.charecter.width > this.block.x
+        && (this.charecter.x + this.charecter.width > this.block.x + this.block.sWidth / 2
         && this.charecter.y - this.charecter.gravity < this.block.y + this.block.sHeight
-        && this.charecter.x < this.block.x + this.block.sWidth)) {
+        && this.charecter.x < this.block.x + this.block.sWidth - this.block.sWidth / 2)) {
             console.log(this.charecter.x, this.charecter.y);
             console.log(this.charecter.width, this.charecter.height);
             window.cancelAnimationFrame(this.charFrameId);
